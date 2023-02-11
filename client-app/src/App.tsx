a3678fd87e9b89037b164d7e5087e706ffdc09a0
Tastyfishnomnomnom <sonner_borg@hotmail.com>
2023-02-11 15:17:45 +0100

    This is the 3rd section

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,32 +2,32 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { Header } from 'semantic-ui-react';
-import List from 'semantic-ui-react/dist/commonjs/elements/List';
-import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
-
+import { List, Item, Button } from 'semantic-ui-react';
+import ListItem from 'semantic-ui-react/dist/commonjs/elements/List/ListItem';
+import Header from 'semantic-ui-react/dist/commonjs/elements/Header';
 
 function App() {
-  const[activities, setActivities] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
-      .then(response => {
-        console.log(response)
-        setActivities(response.data);
-      })
-  },[])
+  const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+      axios.get("http://localhost:5000/api/activities")
+        .then(response => {
+          setActivities(response.data);
+        })
+
+    }, [])
 
   return (
-    <div >
-        <Header as='h2' icon='users' content='Reactivities'/>
-        <List>
-          {activities.map((activity: any) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
+    <div>
+      <Header as='h2' icon = 'users' content = 'reactivities' />
+      <List>
+        {activities.map((thing : any) => (
+          <List.Item key = {thing.id}>
+              {thing.title}
+          </List.Item>
+        ))}
+      </List>
     </div>
   );
 }
